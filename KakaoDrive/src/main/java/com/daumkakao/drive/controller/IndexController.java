42ff614cdc9750ff4ffd30ef658b219dfac0828a
minho <minho@daumcorp.com>
2015-03-31 11:40:39 +0900

    context menu 수정

--- KakaoDrive/src/main/java/com/daumkakao/drive/controller/IndexController.java
+++ KakaoDrive/src/main/java/com/daumkakao/drive/controller/IndexController.java
@@ -1,19 +1,19 @@
 package com.daumkakao.drive.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController {
 
 	private static final Logger LOG = Logger.getLogger(IndexController.class);
 	
-	//@RequestMapping(value = "/")
-	public ModelAndView index() {
-		ModelAndView mav = new ModelAndView("index.html");
-		return mav;
+	@RequestMapping(value = "/")
+	public String index() {
+		return "forward:/index.html";
 	}
 	
 }
